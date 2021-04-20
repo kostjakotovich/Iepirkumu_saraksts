1dc37b5863de758eaa104ee3d0916013d5b975a3
kostjakotovich <kostja.kotovich@gmail.com>
2021-04-20 16:50:48 +0300

    Pievienoju tabulu ar jaunajam precem

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-let biblioteka = []
+let preces = []
 document.getElementById("pievienotPreci").addEventListener('click', Poga)
 function Poga(){
     let prece = {nosaukums: nosaukums.value, daudzums: daudzums.value};
     nosaukums.value = "";
     daudzums.value=  "";
     
-    biblioteka.push(prece);
+    preces.push(prece);
+
+    render();
 
 }
 
 function render() {
+    let biblioteka = document.getElementById('biblioteka');
+
+    for(let i = 0; i < biblioteka.clientHeight; i++) {
+        let prece = `
+    <div class="prece">
+        <h3>Prece: dators</h3>
+        <h4>Daudzums: 5</h4>
+    </div>`;
+    biblioteka.innerHTML = prece;
+    }
     
 }
