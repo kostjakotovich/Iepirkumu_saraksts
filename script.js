29ce5a160e058b58fd5fd8a640fae0140af6bc69
kostjakotovich <kostja.kotovich@gmail.com>
2021-04-20 15:58:35 +0300

    Izmainas js faila,pirmie soli

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("pievienotPreci").addEventListener('click', Poga)
+function Poga(){
+    let list = {nosaukums: nosaukums.value, daudzums: daudzums.value};
+    nosaukums.value = "";
+    daudzums.value=  "";
+
+}
